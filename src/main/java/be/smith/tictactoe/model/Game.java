bbd48a1449765a44c1991bb8404e4897a4e1477e
Agent Smith <smith@matrix.com>
2020-12-13 17:02:59 +0100

    init game board

--- src/main/java/be/smith/tictactoe/model/Game.java
+++ src/main/java/be/smith/tictactoe/model/Game.java
@@ -3,12 +3,18 @@ package be.smith.tictactoe.model;
 public class Game {
 
     private Character turn;
+    private Character[][] board;
 
     public Game() {
         turn = 'X';
+        board = new Character[3][3];
     }
 
     public Character getTurn() {
         return turn;
+    }
+
+    public Character[][] getBoard() {
+        return board;
     }
 }
--- src/test/java/be/smith/tictactoe/model/GameTest.java
+++ src/test/java/be/smith/tictactoe/model/GameTest.java
@@ -9,4 +9,29 @@ public class GameTest {
         Game game = new Game();
         Assert.assertEquals("X doesn't go first", new Character('X'), game.getTurn());
     }
+
+    @Test
+    public void boardShouldHave3RowsOf3Columns() {
+        Game game = new Game();
+        int rowCount = 0;
+        for (Character[] rows : game.getBoard()) {
+            rowCount += 1;
+            int columnCount = 0;
+            for (Character position : rows) {
+                columnCount += 1;
+            }
+            Assert.assertEquals("Board doesn't have 3 colums", 3, columnCount);
+        }
+        Assert.assertEquals("Board doesn't have 3 rows", 3, rowCount);
+    }
+
+    @Test
+    public void allPositionsShouldBeNullAtBeginning() {
+        Game game = new Game();
+        for(Character[] rows : game.getBoard()) {
+            for (Character position : rows) {
+                Assert.assertNull("Positions are not all null at beginning", position);
+            }
+        }
+    }
 }
